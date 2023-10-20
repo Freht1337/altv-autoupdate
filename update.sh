a25628af5b421c4ef4eff9210622f53907fa8598
Freht <109217315+Freht1337@users.noreply.github.com>
2023-10-20 18:17:17 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
+BRANCH="release"
+
 rm -r AltV.Net.Host.dll # delete core modules folder
 rm -r AltV.Net.Host.runtimeconfig.json
 rm -r altv-server
@@ -11,23 +13,23 @@ echo "[DELETE] deleted data folder"
 
 ## core server files
 
-wget -q --show-progress https://cdn.alt-mp.com/coreclr-module/release/x64_linux/AltV.Net.Host.dll # download module
+wget -q --show-progress https://cdn.alt-mp.com/coreclr-module/$BRANCH/x64_linux/AltV.Net.Host.dll # download module
 #echo "downloaded altv.net.host.dll"
 
-wget -q --show-progress https://cdn.alt-mp.com/coreclr-module/release/x64_linux/AltV.Net.Host.runtimeconfig.json # download module
+wget -q --show-progress https://cdn.alt-mp.com/coreclr-module/$BRANCH/x64_linux/AltV.Net.Host.runtimeconfig.json # download module
 #echo "downloaded altv.net.host.runtimeconfig.json"
 
-wget -q --show-progress https://cdn.alt-mp.com/server/release/x64_linux/altv-server
+wget -q --show-progress https://cdn.alt-mp.com/server/$BRANCH/x64_linux/altv-server
 #echo "downloaded main altv-server file"
 
 ## core modules
 mkdir "modules"
 
-wget -q --show-progress https://cdn.alt-mp.com/coreclr-module/release/x64_linux/modules/libcsharp-module.so # download module
+wget -q --show-progress https://cdn.alt-mp.com/coreclr-module/$BRANCH/x64_linux/modules/libcsharp-module.so # download module
 mv libcsharp-module.so ./modules # move module
 #echo "downloaded libcsharp-module.so"
 
-wget -q --show-progress https://cdn.alt-mp.com/js-bytecode-module/release/x64_linux/modules/libjs-bytecode-module.so
+wget -q --show-progress https://cdn.alt-mp.com/js-bytecode-module/$BRANCH/x64_linux/modules/libjs-bytecode-module.so
 mv libjs-bytecode-module.so ./modules # move module
 #echo "downloaded libjs-bytecode-module.so"
 cd "./modules"
@@ -35,11 +37,11 @@ cd "./modules"
 ## js modules
 mkdir "js-module"
 
-wget -q --show-progress https://cdn.alt-mp.com/js-module/release/x64_linux/modules/js-module/libjs-module.so # download module
+wget -q --show-progress https://cdn.alt-mp.com/js-module/$BRANCH/x64_linux/modules/js-module/libjs-module.so # download module
 mv libjs-module.so ./js-module # move module
 #echo "downloaded libjs-module.so"
 
-wget -q --show-progress https://cdn.alt-mp.com/js-module/release/x64_linux/modules/js-module/libnode.so.108 # download module
+wget -q --show-progress https://cdn.alt-mp.com/js-module/$BRANCH/x64_linux/modules/js-module/libnode.so.108 # download module
 mv libnode.so.108 ./js-module # move module
 #echo "downloaded libjs-module.so"
 cd ".."
@@ -48,21 +50,21 @@ cd ".."
 # data files
 mkdir "data"
 
-wget -q --show-progress https://cdn.alt-mp.com/data/release/data/vehmodels.bin # download data
+wget -q --show-progress https://cdn.alt-mp.com/data/$BRANCH/data/vehmodels.bin # download data
 mv vehmodels.bin ./data # move module
 
-wget -q --show-progress https://cdn.alt-mp.com/data/release/data/vehmods.bin # download data
+wget -q --show-progress https://cdn.alt-mp.com/data/$BRANCH/data/vehmods.bin # download data
 mv vehmods.bin ./data # move module
 
-wget -q --show-progress https://cdn.alt-mp.com/data/release/data/clothes.bin # download data
+wget -q --show-progress https://cdn.alt-mp.com/data/$BRANCH/data/clothes.bin # download data
 mv clothes.bin ./data # move module
 
-wget -q --show-progress https://cdn.alt-mp.com/data/release/data/pedmodels.bin # download data
+wget -q --show-progress https://cdn.alt-mp.com/data/$BRANCH/data/pedmodels.bin # download data
 mv pedmodels.bin ./data # move module
 
-wget -q --show-progress https://cdn.alt-mp.com/data/release/data/rpfdata.bin # download data
+wget -q --show-progress https://cdn.alt-mp.com/data/$BRANCH/data/rpfdata.bin # download data
 mv rpfdata.bin ./data # move module
 
-wget -q --show-progress https://cdn.alt-mp.com/data/release/data/weaponmodels.bin # download data
+wget -q --show-progress https://cdn.alt-mp.com/data/$BRANCH/data/weaponmodels.bin # download data
 mv weaponmodels.bin ./data # move module
 echo "[UPDATE] Server updated successfully"
